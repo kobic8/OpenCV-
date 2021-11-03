255713273e25c7071f31242edcbc55c70dc0e573
Kobi Cohen <kobic8@gmail.com>
2021-11-03 08:48:31 +0200

    addimage blend funstion and move imgs to DB

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def read_write_image():
     flag = -1  # loads including alpha channel
     flag = 0  # loads a color image
 
-    filename = 'img_for_obj_detection.ppm'
+    filename = 'DataBase/img_for_obj_detection.ppm'
 
     img = cv2.imread(filename, flag)
     # show image
@@ -24,12 +24,44 @@ def read_write_image():
     elif key == 27:  # ESC
         cv2.destroyAllWindows()
 
+
+def read_with_matplotlib():
+    import numpy as np
+    import matplotlib
+    from matplotlib import pyplot as plt
+    # read image.
+    print(matplotlib.get_backend())
+    filename = 'DataBase/img_for_obj_detection.png'
+    img = cv2.imread(filename, 0)
+    # show image
+    plt.imshow(img, cmap='gray', interpolation='bicubic')
+    plt.xticks([]), plt.yticks([])  # hide tick values for X Y
+    plt.show()
+
+
+def image_blending():
+    img1 = cv2.imread("/home/yakovc/PycharmProjects/opencv_tut/DataBase/pic_1_openCV.png") # pic_1_openCV.png")
+    img2 = cv2.imread("DataBase/pic_2_bot.png")
+
+    dim1 = list(img1.shape)[:-1]
+    dim2 = list(img2.shape)[:-1]
+    if not dim1 == dim2:
+        print("dimensions of image1 are: {}".format(dim1))
+        print("dimensions of image2 are: {}".format(dim2))
+        print("reverse way is: {}".format(dim1[::-1]))
+        # img2 = cv2.resize(img2, [228, 286])
+        img2 = cv2.resize(img2, dim1[::-1])
+    dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
+
+    cv2.imshow('dst', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
     # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     # print(cv2.__version__)
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    read_write_image()
+    # read_with_matplotlib()
+    image_blending()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
